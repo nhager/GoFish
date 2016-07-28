a22e1a1f2b43646c54e875b4af67a84283320c5e
CHAU <jc4101@att.com>
2016-07-28 09:59:15 -0400

    Location and marking working locally

--- app/src/main/java/com/example/nh612u/gofish/LocationTest.java
+++ app/src/main/java/com/example/nh612u/gofish/LocationTest.java
@@ -1,7 +1,156 @@
 package com.example.nh612u.gofish;
 
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by jc4101 on 7/27/2016.
  */
-public class LocationTest {
+public class LocationTest extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private TextView mLong;
+    private TextView mLat;
+
+    private Location mCurrentLocation;
+    private LocationRequest mLocationRequest;
+    private boolean mRequestingLocationUpdates;
+    protected String mLastUpdateTime;
+    private Marker mMarker;
+
+    private GoogleMap mMap;
+
+    private static final long UPDATE = 10000;
+    private static final long FASTEST_UPDATE_INTERVAL = UPDATE / 2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_test);
+
+        mLat = (TextView) findViewById(R.id.latitude);
+        mLong = (TextView) findViewById(R.id.longitude);
+
+        mRequestingLocationUpdates = false;
+        mLastUpdateTime = "";
+
+        if (mGoogleApiClient == null) {
+            mRequestingLocationUpdates = true;
+            buildGoogleApiClient();
+        }
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        createLocationRequest();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE);
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        if (mCurrentLocation == null) {
+            try {
+                mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                updateUi();
+            } catch (SecurityException se) {
+
+            }
+        }
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        try {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (LocationListener) this);
+        } catch (SecurityException se) {
+
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("", "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i("", "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        updateUi();
+    }
+
+    protected void updateUi() {
+        mLong.setText(Double.toString(mCurrentLocation.getLongitude()));
+        mLat.setText(Double.toString(mCurrentLocation.getLatitude()));
+        if (mMarker == null)
+            mMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude())).title("Marker"));
+        else
+            mMarker.setPosition(new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude()));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+            mMap = map;
+    }
 }
