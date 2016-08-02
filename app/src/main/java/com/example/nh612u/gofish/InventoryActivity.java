5cffdc1e6b5a142134b0931137d0d4155e5f4391
DavidPurcell <d.purcell222@gmail.com>
2016-08-02 01:41:28 -0400

    UI for working with inventory.
    Basic version, mostly working.

--- app/src/main/java/com/example/nh612u/gofish/InventoryActivity.java
+++ app/src/main/java/com/example/nh612u/gofish/InventoryActivity.java
@@ -1,17 +1,18 @@
 package com.example.nh612u.gofish;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class InventoryActivity extends AppCompatActivity {
+
+    Spinner eventSelect;
+    Spinner typeSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,21 @@ public class InventoryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        String[] events = getEvents();
+        eventSelect = (Spinner) findViewById(R.id.typeFilterSpinner);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, events);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        eventSelect.setAdapter(dataAdapter);
+
+        final Button createItemButton = (Button) findViewById(R.id.createItemButton);
+        createItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(InventoryActivity.this, CreateItemActivity.class));
+            }
+        });
+
         final Button inventoryButton = (Button) findViewById(R.id.assignItemButton);
         inventoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +43,19 @@ public class InventoryActivity extends AppCompatActivity {
                 startActivity(new Intent(InventoryActivity.this, AssignItemActivity.class));
             }
         });
+    }
+
+    //TODO: Get user list from database.
+    //TODO: User user object instead of string.
+    public String[] getTypes(){
+        String[] strs = {"Kayak","Buffalo","Candy"};
+        return strs;
+    }
+
+    //TODO: Get user list from database.
+    //TODO: User user object instead of string.
+    public String[] getEvents(){
+        String[] strs = {"David Purcell","Phillp-a","Bilanco"};
+        return strs;
     }
 }
